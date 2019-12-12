bbf84cd9a0121cfce3301aad808c5248147d44a4
seonju <seonju1022@gmail.com>
2019-12-12 15:24:09 +0900

    understanding redirect to web site

--- hello.py
+++ hello.py
@@ -79,5 +79,13 @@ def pong():
     keyword = request.form.get('keyword')
     return render_template('pong.html', keyword=keyword)
 
+@app.route('/naver')
+def naver():
+    return render_template('naver.html')
+
+@app.route('/google')
+def google():
+    return render_template('google.html')
+
 if __name__ == "__main__":
     app.run(debug=True)     # .py 파일을 python hello.py 명령어로 실행시키기 위한 작업   # 자동으로 서버에 반영해주는 역할도 함
